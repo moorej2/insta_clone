3802fe2135e6872f606bfc58a8a274307462bcbc
moorej2 <moorejjeremy@gmail.com>
2022-07-31 17:07:04 -0400

    posts migration

--- db/migrate/20220731210235_create_posts.rb
+++ db/migrate/20220731210235_create_posts.rb
@@ -1,7 +1,9 @@
 class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
-
+      t.string :image
+      t.boolean :active
+      t.references :account
       t.timestamps
     end
   end
